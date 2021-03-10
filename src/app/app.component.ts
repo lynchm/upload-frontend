064875c04d03ca429a90263204a2674a90e2eaa4
lynchm <lmachabango@gmail.com>
2021-03-10 08:52:12 +0200

    proxy.conf.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'upload';
+export class AppComponent implements OnInit {
+  uploadedFileList: Array < File > ;
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+
+  }
+  upload() {
+    let formData = new FormData();
+    for (var i = 0; i < this.uploadedFileList.length; i++) {
+        formData.append("uploads[]", this.uploadedFileList[i], this.uploadedFileList[i].name);
+    }
+    this.http.post('/api/upload', formData)
+        .subscribe((response) => {
+            console.log('response received is ', response);
+        })
+  }
+  fileChange(element) {
+    this.uploadedFileList = element.target.files;
+   }
 }
+
